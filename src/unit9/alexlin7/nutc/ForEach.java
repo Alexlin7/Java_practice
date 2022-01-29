cfcfa444e0d9f6635b60bb737ac9782983772f52
Alexlin7 <alitw@outlook.com>
2022-01-29 18:38:27 +0800

    typeSafe

--- src/unit9/alexlin7/nutc/ForEach.java
+++ src/unit9/alexlin7/nutc/ForEach.java
@@ -7,8 +7,8 @@ public class ForEach {
 	public static void main(String[] args) {
 		var names = Arrays.asList("Justin", "Monica", "Irene");
 		names.forEach(name -> System.out.println(name));
-		new HashSet(names).forEach(name -> System.out.println(name));
-		new ArrayDeque(names).forEach(name -> System.out.println(name));
+		new HashSet<String>(names).forEach(name -> System.out.println(name));
+		new ArrayDeque<String>(names).forEach(name -> System.out.println(name));
 //		forEach(names);
 //		forEach(new HashSet(names));
 //		forEach(new ArrayDeque(names));
