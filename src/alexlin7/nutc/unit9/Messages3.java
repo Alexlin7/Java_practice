d40203eeb414d9d6a34491a046654dcfd2a33b02
Alexlin7 <alitw@outlook.com>
2022-02-10 20:57:52 +0800

    修改2的排版 新增3(lambda)的練習

--- src/alexlin7/nutc/unit9/Messages3.java
+++ src/alexlin7/nutc/unit9/Messages3.java
@@ -1,14 +1,17 @@
 package alexlin7.nutc.unit9;
 
-import java.util.*;
+import  java.util.*;
 
-public class Messages2 {
-    public static void main(String[] args) {
-        var messages = new TreeMap<String, String>();
+public class Messages3 {
+    public static  void main(String[] args) {
+        var messages = new TreeMap<String, String>(
+                (s1, s2) -> -s1.compareTo(s2)
+        );
         messages.put("Justin", "Hello!給Justin的訊息");
         messages.put("Monica", "給Monica的幹話");
         messages.put("Irena", "我愛Irena!!!!");
-
         System.out.println(messages);
+
+
     }
 }
